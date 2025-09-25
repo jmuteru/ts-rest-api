9da988a3264a80469a5fde3e79d41e3aa9959650
jmuteru <jeffmuteru@gmail.com>
2025-09-26 01:24:38 +0300

    Setup: middleware configurations

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const config ={
-    PORT:process.env.PORT || 3000
+    PORT:process.env.PORT || 3000,
+    NODE_ENV:process.env.NODE_ENV,
+    WHITELIST_ORIGINS:['https://docs.ts-rest-api.jmuteru.com']
 }
 
 export default config
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,51 @@
 import express from "express"
 import config from "./config"
+import cors from "cors"
+import { CorsOptions } from "cors"
+import cookieParser from "cookie-parser"
+import compression from "compression"
+import helmet from "helmet"
 
-const app  = express()
+const app = express()
 
+//cors options
+const corsOptions:CorsOptions = {
+    origin(origin, callback) {
+        if (config.NODE_ENV === 'development'|| !origin || config.WHITELIST_ORIGINS.includes(origin)) {
+            callback(null, true)
 
-app.listen(config.PORT, ()=>{
+        }
+        else{
+            //reject origins not whitelisted
+            callback(new Error(`CORS Error:${origin}is not allowed by CORS`),false)
+        }
+    },
+}
+//cors middleware
+app.use(cors(corsOptions))
+
+//JSON request body parsing
+
+app.use(express.json())
+//enable url-encoded requests
+ app.use(express.urlencoded({extended:true})) 
+ app.use(cookieParser())
+
+ //response compression to reduce payload size & improve performance
+ app.use(compression ({
+    threshold:1024,//compress responses larger than 1kb
+ }))
+
+ //rate limiting middleware
+
+ //use helmet to enhance security by setting various HTTP headers
+ app.use(helmet())
+app.get("/", (req, res) => {
+    res.json({
+        message: "This works"
+    })
+})
+
+app.listen(config.PORT, () => {
     console.log(`server is running on port :${config.PORT}`)
 })
